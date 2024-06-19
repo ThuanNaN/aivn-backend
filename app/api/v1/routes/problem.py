c221cfe1ae604b08d9aad6e21c7c6b47ee85415e
ThuanNaN <duongthuan1445@gmail.com>
2024-06-19 22:19:02 +0700

    add logger

--- app/api/v1/routes/problem.py
+++ app/api/v1/routes/problem.py
@@ -20,14 +20,16 @@ logger = Logger("routes/problem", log_file="problem.log")
 
 @router.post("/problem", description="Add a new problem")
 async def create_problem(problem: ProblemSchema):
-    problem_dict = problem.model_dump()
     try:
+        problem_dict = problem.model_dump()
         new_problem = await add_problem(problem_dict)
+
     except Exception as e:
         logger.error(f"Error when add problem: {e}")
         return ErrorResponseModel(error="An error occurred.",
                                   message="Problem was not added.",
                                   code=400)
+    
     return ResponseModel(data=[new_problem],
                          message="Problem added successfully.",
                          code=200)
@@ -53,35 +55,44 @@ async def get_problems():
 
 @router.get("/problem/{id}", description="Retrieve a problem with a matching ID")
 async def get_problem(id):
-    problem = await retrieve_problem(id)
-    if problem:
-        return ResponseModel(data=[problem],
-                             message="Problem retrieved successfully.",
-                             code=200)
-    return ErrorResponseModel(error="An error occurred.",
-                              message="Problem does not exist.",
-                              code=404)
+    try:
+        problem = await retrieve_problem(id)
+        if problem:
+            return ResponseModel(data=[problem],
+                                message="Problem retrieved successfully.",
+                                code=200)
+    except Exception as e:
+        logger.error(f"Error when retrieve problem: {e}")
+        return ErrorResponseModel(error="An error occurred.",
+                                  message="Problem was not retrieved.",
+                                  code=404)
 
 
 @router.patch("/problem/{id}", description="Update a problem with a matching ID")
 async def update_problem_data(id: str, data: UpdateProblemSchema = Body(...)):
-    updated = await update_problem(id, data.model_dump())
-    if updated:
-        return ResponseModel(data=[],
-                             message="Problem data updated successfully.",
-                             code=200)
-    return ErrorResponseModel(error="An error occurred.",
-                              message="Problem data was not updated.",
-                              code=404)
+    try:
+        updated = await update_problem(id, data.model_dump())
+        if updated:
+            return ResponseModel(data=[],
+                                message="Problem data updated successfully.",
+                                code=200)
+    except Exception as e:
+        logger.error(f"Error when update problem: {e}")
+        return ErrorResponseModel(error="An error occurred.",
+                                message="Problem data was not updated.",
+                                code=404)
 
 
 @router.delete("/problem/{id}", description="Delete a problem with a matching ID")
 async def delete_problem_data(id: str):
-    deleted = await delete_problem(id)
-    if deleted:
-        return ResponseModel(data=[],
-                             message="Problem deleted successfully.",
-                             code=200)
-    return ErrorResponseModel(error="An error occurred.",
-                              message="Problem was not deleted.",
-                              code=404)
+    try:
+        deleted = await delete_problem(id)
+        if deleted:
+            return ResponseModel(data=[],
+                                message="Problem deleted successfully.",
+                                code=200)
+    except Exception as e:
+        logger.error(f"Error when delete problem: {e}")
+        return ErrorResponseModel(error="An error occurred.",
+                                message="Problem was not deleted.",
+                                code=404)
