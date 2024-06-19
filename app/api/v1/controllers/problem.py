92753ea6389b1de154e98dc231a38c338d8b77f0
ThuanNaN <duongthuan1445@gmail.com>
2024-06-19 22:01:53 +0700

    add logger

--- app/api/v1/controllers/problem.py
+++ app/api/v1/controllers/problem.py
@@ -1,10 +1,13 @@
 from app.core.database import mongo_db
+from app.utils.logger import Logger
 from bson.objectid import ObjectId
+
+logger = Logger("controllers/problem", log_file="problem.log")
 
 try:
     problem_collection = mongo_db["problems"]
 except Exception as e:
-    print(f"An error occurred while trying to access the database: {e}")
+    logger.error(f"Error when connect to collection: {e}")
     exit(1)
 
 # helper
@@ -23,43 +26,58 @@ def problem_helper(problem) -> dict:
 
 # Create a new problem
 async def add_problem(problem_data: dict) -> dict:
-    problem = await problem_collection.insert_one(problem_data)
-    new_problem = await problem_collection.find_one({"_id": problem.inserted_id})
-    return problem_helper(new_problem)
+    try:
+        problem = await problem_collection.insert_one(problem_data)
+        new_problem = await problem_collection.find_one({"_id": problem.inserted_id})
+        return problem_helper(new_problem)
+    except Exception as e:
+        logger.error(f"Error when add problem: {e}")
 
 
 # Retrieve all problems
 async def retrieve_problems():
-    problems = []
-    async for problem in problem_collection.find():
-        problems.append(problem_helper(problem))
-    return problems
+    try:
+        problems = []
+        async for problem in problem_collection.find():
+            problems.append(problem_helper(problem))
+        return problems
+    except Exception as e:
+        logger.error(f"Error when retrieve problems: {e}")
 
 
 # Retrieve a problem with a matching ID
 async def retrieve_problem(id: str) -> dict:
-    problem = await problem_collection.find_one({"_id": ObjectId(id)})
-    if problem:
-        return problem_helper(problem)
+    try:
+        problem = await problem_collection.find_one({"_id": ObjectId(id)})
+        if problem:
+            return problem_helper(problem)
+    except Exception as e:
+        logger.error(f"Error when retrieve problem: {e}")
 
 
 # Update a problem with a matching ID
 async def update_problem(id: str, data: dict):
-    if len(data) < 1:
-        return False
-    problem = await problem_collection.find_one({"_id": ObjectId(id)})
-    if problem:
-        updated_problem = await problem_collection.update_one(
-            {"_id": ObjectId(id)}, {"$set": data}
-        )
-        if updated_problem:
-            return True
-        return False
+    try:
+        if len(data) < 1:
+            return False
+        problem = await problem_collection.find_one({"_id": ObjectId(id)})
+        if problem:
+            updated_problem = await problem_collection.update_one(
+                {"_id": ObjectId(id)}, {"$set": data}
+            )
+            if updated_problem:
+                return True
+            return False
+    except Exception as e:
+        logger.error(f"Error when update problem: {e}")
 
 
 # Delete a problem from the database
 async def delete_problem(id: str):
-    problem = await problem_collection
-    if problem:
-        await problem_collection.delete_one({"_id": ObjectId(id)})
-        return True
+    try:
+        problem = await problem_collection
+        if problem:
+            await problem_collection.delete_one({"_id": ObjectId(id)})
+            return True
+    except Exception as e:
+        logger.error(f"Error when delete problem: {e}")
