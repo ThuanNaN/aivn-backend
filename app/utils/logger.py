09646b557e6df3cd328f935ef7ab108caf01ab19
ThuanNaN <duongthuan1445@gmail.com>
2024-06-21 01:47:04 +0700

    fix: stream log

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Union
 from pathlib import Path
 import logging
 from logging.handlers import RotatingFileHandler
@@ -6,16 +7,22 @@ from logging.handlers import RotatingFileHandler
 LOG_DIR = Path(__file__).resolve().parents[1] / "logs"
 
 class Logger(logging.Logger):
-    def __init__(self, name="", log_level=logging.INFO, log_file=None) -> None:
+    def __init__(self, 
+                 name="logger", 
+                 log_level: Union[int, str] = logging.INFO, 
+                 log_file = None,
+                 stream_handler = True
+                 ) -> None:
         super().__init__(name, log_level)
-        self.get_logger(log_level, log_file)
+        self.get_logger(log_level, log_file, stream_handler)
     
-    def get_logger(self, log_level, log_file):
+    def get_logger(self, log_level, log_file, stream_handler):
         self.setLevel(log_level)
         self._init_formatter()
+
         if log_file is not None:
             self._add_file_handler(LOG_DIR / log_file)
-        else:
+        if stream_handler:
             self._add_stream_handler()
     
     def _init_formatter(self):
