f9eb6431762217942c565c11820950417db3f0ae
ThuanNaN <duongthuan1445@gmail.com>
2024-06-24 01:07:57 +0700

    feat: return testcase

--- app/api/v1/controllers/run_code.py
+++ app/api/v1/controllers/run_code.py
@@ -18,25 +18,33 @@ def text2var(str_input: str) -> dict:
     try:
         exec(str_input, globals, locals)
     except Exception as e:
-        print(e)
-        print("str_input: ", str_input, type(str_input))
+        logger.error(f"Error converting text to variable: {e}")
     return locals
 
 
-def run_testcase(func, test_case: Dict[str, str]) -> dict:
+def run_testcase(func, 
+                 test_case: Dict[str, str], 
+                 return_testcase: bool = False
+                ) -> dict:
     input = text2var(test_case["input"])
     expected_output = text2var("output = "+test_case["expected_output"])["output"]
     testcase_id = str(test_case["testcase_id"])
     testcase_output = {
         "testcase_id": testcase_id,
+        "output": func_output,
         "is_pass": False,
     }
-    func_output = func(**input)
+    try:
+        func_output = func(**input)
+    except Exception as e:
+        func_output = e
+
+    if return_testcase:
+        testcase_output["expected_output"] = expected_output
+        
     if func_output == expected_output:
         testcase_output["is_pass"] = True
-    else:
-        print(func_output)
-        print(expected_output)
+        
     return testcase_output
 
 
