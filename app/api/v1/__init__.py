c9f05c109a2b150af97cf4d64f4e6050d614c628
ThuanNaN <duongthuan1445@gmail.com>
2024-06-23 23:55:52 +0700

    update: routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.api.v1.routes.build_chart import router as build_chart_router
 from app.api.v1.routes.problem import router as problem_router
 from app.api.v1.routes.code import router as code_router
+from app.api.v1.routes.submission import router as submission_router
 
 v1_router = APIRouter(prefix="/v1", tags=["API v1"])
 
@@ -11,4 +12,5 @@ async def check_health():
 
 v1_router.include_router(build_chart_router, tags=["Build Chart"])
 v1_router.include_router(problem_router, tags=["Problem"])
-v1_router.include_router(code_router, tags=["Code"])
+v1_router.include_router(code_router, prefix="/code", tags=["Code"])
+v1_router.include_router(submission_router, prefix="/code", tags=["Code"])
