5c97c768dfafa1008f75cb1d6a8b0d13f5d770b6
ThuanNaN <duongthuan1445@gmail.com>
2024-06-23 23:55:09 +0700

    feat: exc code

--- app/api/v1/controllers/run_code.py
+++ app/api/v1/controllers/run_code.py
@@ -1,13 +1,50 @@
-from typing import List
-import subprocess
+from typing import List, Dict
+from app.utils.logger import Logger
+
+logger = Logger("controllers/run_code", log_file="run_code.log")
 
 
+def buld_py_funct(py_func: str) -> callable:
+    locals = {}
+    globals = {}
+    exec(py_func, globals, locals)
+    func = next(iter(locals.values()))
+    return func
 
-def run_py_funct(py_func: str, args: List[str]):
+
+def text2var(str_input: str) -> dict:
+    locals = {}
+    globals = {}
+    try:
+        exec(str_input, globals, locals)
+    except Exception as e:
+        print(e)
+        print("str_input: ", str_input, type(str_input))
+    return locals
 
 
-    pass
+def run_testcase(func, test_case: Dict[str, str]) -> dict:
+    input = text2var(test_case["input"])
+    expected_output = text2var("output = "+test_case["expected_output"])["output"]
+    testcase_id = str(test_case["testcase_id"])
+    testcase_output = {
+        "testcase_id": testcase_id,
+        "is_pass": False,
+    }
+    func_output = func(**input)
+    if func_output == expected_output:
+        testcase_output["is_pass"] = True
+    else:
+        print(func_output)
+        print(expected_output)
+    return testcase_output
 
 
-def test_py_funct(py_func: str, test_case: List[str]):
-    pass
+def test_py_funct(py_func: str, 
+                  test_case: List[Dict[str, str]]
+                  ) -> List[dict]:
+    func = buld_py_funct(py_func)
+    testcase_outputs = []
+    for case in test_case:
+        testcase_outputs.append(run_testcase(func, case))
+        return testcase_outputs
--- app/api/v1/routes/code.py
+++ app/api/v1/routes/code.py
@@ -1,13 +1,33 @@
 from fastapi import APIRouter, Body
-
+from app.schemas.code import CodeSchema, ResponseModel, ErrorResponseModel
+from app.api.v1.controllers.problem import retrieve_problem
+from app.api.v1.controllers.run_code import test_py_funct
+from app.utils.logger import Logger
 
 router = APIRouter()
+logger = Logger("routes/code", log_file="code.log")
+
 
 @router.post("/run", description="Run code from code block (string)")
-async def run_code(code: str = Body(...)):
-    return {"message": "Code run successfully."}
+async def run_code(code_inputs: CodeSchema = Body(...)):
+    problem = await retrieve_problem(code_inputs.problem_id)
+    if not problem:
+        logger.error(f"Problem with ID {code_inputs.problem_id} not found.")
+        return ErrorResponseModel(error="An error occurred.",
+                                  message="Problem not found.",
+                                  code=404)
 
+    public_testcases = problem["public_testcases"]
+    private_testcases = problem["private_testcases"]
 
-@router.post("/submit", description="Submit code to a problem")
-async def submit_code():
-    return {"message": "Code submitted successfully."}
+    # Run code
+    public_testcases_results = test_py_funct(code_inputs.code, public_testcases)
+    private_testcases_results = test_py_funct(code_inputs.code, private_testcases)
+
+    return ResponseModel(
+        data={
+            "public_testcases_results": public_testcases_results,
+            "private_testcases_results": private_testcases_results,
+        },
+        message="Code run successfully.",
+        code=200)
