a2b3f85f5d008981012fe706968f8c1b43167b55
ThuanNaN <duongthuan1445@gmail.com>
2024-06-26 20:17:29 +0700

    Update user schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,25 +1,30 @@
-from typing import Optional
+from typing import Optional, Union
 from datetime import datetime
 from pydantic import BaseModel
 
 
 class UserSchema(BaseModel):
-    username: str
-    email: str
-    created_at: datetime = datetime.now().isoformat()
-    updated_at: datetime = datetime.now().isoformat()
+    clerk_user_id: str | None = None
+    email: str | None = None
+    username: str | None = None
+    role: str | None = None
+    avatar: str | None = None
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
 
 
 class UpdateUserSchema(BaseModel):
     username: Optional[str]
-    email: Optional[str]
-    updated_at: datetime = datetime.now().isoformat()
+    role: Optional[str]
+    avatar: Optional[str]
+    updated_at: datetime = datetime.now()
 
 
 class ResponseModel(BaseModel):
-    data: list
+    data: Union[list, dict]
     message: str
     code: int
+
 
 class ErrorResponseModel(BaseModel):
     error: str
