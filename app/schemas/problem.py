f910126f769e9223538b5e48af8ba5c173f15e35
ThuanNaN <duongthuan1445@gmail.com>
2024-06-30 03:07:26 +0700

    add choices to Update scheme

--- app/schemas/problem.py
+++ app/schemas/problem.py
@@ -9,14 +9,28 @@ class TestCase(BaseModel):
     expected_output: str
 
 
+class Choice(BaseModel):
+    choice_id: UUID = Field(default_factory=uuid4)
+    answer: str
+    is_correct: bool
+
+
 class ProblemSchema(BaseModel):
     title: str
     description: str
     index: int
     slug: str
-    code_template: str
-    public_testcases: List[TestCase]
-    private_testcases: List[TestCase]
+    
+    # >>> code problems
+    code_template: str = None
+    public_testcases: List[TestCase] = None
+    private_testcases: List[TestCase] = None
+    # >>> code problems
+
+    # >>> choice problems
+    choices: List[Choice] = None
+    # >>> choice problems
+
     created_at: datetime = datetime.now()
     updated_at: datetime = datetime.now()
 
@@ -29,11 +43,14 @@ class UpdateProblemSchema(BaseModel):
     code_template: Optional[str]
     public_testcases: Optional[List[TestCase]]
     private_testcases: Optional[List[TestCase]]
+    choices: Optional[List[Choice]]
     updated_at: datetime = datetime.now()
+
 
 class OrderSchema(BaseModel):
     problem_id: str
     index: int
+
 
 class ResponseModel(BaseModel):
     data: Union[dict, List[dict]]
