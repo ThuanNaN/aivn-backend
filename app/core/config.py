121d0df78c38b1da5b14526b5ea899492a4abf53
ThuanNaN <duongthuan1445@gmail.com>
2024-07-10 05:32:43 +0700

    update project name

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Python Test Case API"
+    PROJECT_NAME: str = "Python Coding API"
 
 settings = Settings()
