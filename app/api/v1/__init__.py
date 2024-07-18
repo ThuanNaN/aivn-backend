6eb1cf401f01ba40d6a1d189834496fffb09620b
ThuanNaN <duongthuan1445@gmail.com>
2024-07-18 16:20:58 +0700

    add exam_problem

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,12 +4,14 @@ from app.api.v1.routes.contest import router as contest_router
 from app.api.v1.routes.exam import router as exam_router
 from app.api.v1.routes.category import router as category_router
 from app.api.v1.routes.problem import router as problem_router
+from app.api.v1.routes.exam_problem import router as exam_problem_router
 from app.api.v1.routes.code import router as code_router
 from app.api.v1.routes.timer import router as timer_router
 from app.api.v1.routes.submission import router as submission_router
 from app.core.security import (
     is_authenticated,
-    is_aio
+    is_aio,
+    is_admin
 )
 
 router = APIRouter()
@@ -39,6 +41,11 @@ router.include_router(problem_router,
                       #   dependencies=[Depends(is_authenticated)],
                       prefix="/problem",
                       tags=["Problem"])
+
+router.include_router(exam_problem_router,
+                      dependencies=[Depends(is_admin)],
+                      prefix="/exam_problem",
+                      tags=["Exam Problem"])
 
 
 router.include_router(code_router,
