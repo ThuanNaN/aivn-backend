fb3d401064efd2b1f1923e1b8295a30d98693599
ThuanNaN <duongthuan1445@gmail.com>
2024-07-18 16:27:56 +0700

    update setting schemas

--- app/schemas/setting.py
+++ app/schemas/setting.py
@@ -1,14 +1,37 @@
 from pydantic import BaseModel
+from datetime import datetime
+
 
 class SettingSchema(BaseModel):
+    exam_id: str
     duration: int
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
 
-class ResponseModel(BaseModel):
-    data: dict | list
-    message: str
-    code: int
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "exam_id": "1",
+                    "duration": 30,
+                    "created_at": datetime.now(),
+                    "updated_at": datetime.now()
+                }
+            ]
+        }
+    }
 
-class ErrorResponseModel(BaseModel):
-    error: str
-    message: str
-    code: int
+class UpdateSettingSchema(BaseModel):
+    duration: int
+    updated_at: datetime = datetime.now()
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "duration": 50,
+                    "updated_at": datetime.now()
+                }
+            ]
+        }
+    }
