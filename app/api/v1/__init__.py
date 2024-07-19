24d44b1cd12fcdb3fb38650777316038218f32c5
ThuanNaN <duongthuan1445@gmail.com>
2024-07-19 08:55:56 +0700

    rm exam_problem_router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,7 +4,6 @@ from app.api.v1.routes.contest import router as contest_router
 from app.api.v1.routes.exam import router as exam_router
 from app.api.v1.routes.category import router as category_router
 from app.api.v1.routes.problem import router as problem_router
-from app.api.v1.routes.exam_problem import router as exam_problem_router
 from app.api.v1.routes.code import router as code_router
 from app.api.v1.routes.timer import router as timer_router
 from app.api.v1.routes.submission import router as submission_router
@@ -20,7 +19,6 @@ router.include_router(user_router,
                       dependencies=[Depends(is_authenticated)],
                       prefix="/user",
                       tags=["User"])
-
 
 router.include_router(contest_router,
                       dependencies=[Depends(is_authenticated)],
@@ -42,23 +40,15 @@ router.include_router(problem_router,
                       prefix="/problem",
                       tags=["Problem"])
 
-router.include_router(exam_problem_router,
-                      dependencies=[Depends(is_admin)],
-                      prefix="/exam_problem",
-                      tags=["Exam Problem"])
-
-
 router.include_router(code_router,
                       dependencies=[Depends(is_authenticated)],
                       prefix="/code",
                       tags=["Code"])
 
-
 router.include_router(submission_router,
                       dependencies=[Depends(is_authenticated)],
                       prefix="/submission",
                       tags=["Submission"])
-
 
 router.include_router(timer_router,
                       dependencies=[Depends(is_authenticated)],
