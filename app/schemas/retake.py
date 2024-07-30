85e7a78bc9df258cb5cfd39df85087a71c593694
ThuanNaN <duongthuan1445@gmail.com>
2024-07-31 01:14:01 +0700

    update retake schema

--- app/schemas/retake.py
+++ app/schemas/retake.py
@@ -5,15 +5,7 @@ from datetime import datetime
 class RetakeSchema(BaseModel):
     user_clerk_id: str
     created_at: datetime = datetime.now()
-    updated_at: datetime = datetime.now()
 
 
 class RetakeSchemaDB(RetakeSchema):
     exam_id: str
-
-
-# class UpdateRetake(BaseModel):
-#     user_clerk_id: str | None = None
-#     exam_id: str | None = None
-#     updated_at: datetime = datetime.now()
-
