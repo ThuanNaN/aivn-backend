0ac06402d875e93a84c03c4764dea65846d2e1d6
ThuanNaN <duongthuan1445@gmail.com>
2024-08-09 09:40:22 +0700

    clean: import

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
-import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.middleware import LogProcessAndTime
 from app.core.config import settings
 from app.api.v1 import router as v1_router
-from app.utils.logger import Logger
 load_dotenv()
+
 
 def create_application() -> FastAPI:
     app = FastAPI(title=settings.PROJECT_NAME,
-                  #   openapi_url="/api/v1/openapi.json",
-                  #   docs_url="/api/v1/docs"
+                #   openapi_url="/api/v1/openapi.json",
+                #   docs_url="/api/v1/docs"
                   )
     app.add_middleware(
         CORSMiddleware,
@@ -23,5 +22,6 @@ def create_application() -> FastAPI:
     app.add_middleware(LogProcessAndTime)
     app.include_router(v1_router, prefix="/api/v1")
     return app
+
 
 app = create_application()
