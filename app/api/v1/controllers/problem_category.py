2928968ca9e6358f4f0dc457a93d91d9e8913b24
ThuanNaN <duongthuan1445@gmail.com>
2024-08-12 02:26:18 +0700

    feat: upsert problem_category

--- app/api/v1/controllers/problem_category.py
+++ app/api/v1/controllers/problem_category.py
@@ -3,6 +3,7 @@ from typing import List
 from app.core.database import mongo_db
 from app.utils.logger import Logger
 from bson.objectid import ObjectId
+from pymongo import UpdateOne, DeleteOne
 
 logger = Logger("controllers/problem_category", log_file="problem_category.log")
 
@@ -189,6 +190,55 @@ async def delete_all_by_problem_id(problem_id: str) -> bool:
         if deleted_problem_category.deleted_count > 0:
             return True
         return False
+    except Exception as e:
+        logger.error(f"{traceback.format_exc()}")
+        return e
+
+
+async def upsert_problem_category(problem_id: str, new_problem_categories: list) -> dict:
+    """
+    Upsert problem_category to database by problem_id
+    :param id: str
+    :param problem_category_data: dict
+    :return: dict
+    """
+    try:
+        new_problem_categories = [ObjectId_helper(problem_category) for problem_category in new_problem_categories]
+
+        current_problem_categories = await problem_category_collection.find(
+            {
+                "problem_id": ObjectId(problem_id)
+            }
+        ).to_list(length=None)
+
+        current_categories = {problem_category["category_id"] for problem_category in current_problem_categories}
+        new_categories = {problem_category["category_id"] for problem_category in new_problem_categories}
+
+        del_problem_categories = current_categories - new_categories
+        operations = [
+            UpdateOne(
+                {
+                    "problem_id": ObjectId(problem_category["problem_id"]),
+                    "category_id": ObjectId(problem_category["category_id"])
+                },
+                {"$set": problem_category},
+                upsert=True
+            ) for problem_category in new_problem_categories
+        ]
+
+        operations.extend(
+            DeleteOne(
+                {
+                    "problem_id": ObjectId(problem_id),
+                    "category_id": del_problem_category
+                }
+            ) for del_problem_category in del_problem_categories
+        )
+        if operations:
+            result = await problem_category_collection.bulk_write(operations)
+            logger.info(f"Bulk write result: {result.bulk_api_result}")
+        return True
+
     except Exception as e:
         logger.error(f"{traceback.format_exc()}")
         return e
