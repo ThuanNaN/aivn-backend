dd5e2397a6677c579a71e3555f3306dafacb909f
ThuanNaN <duongthuan1445Gmail.com>
2024-09-25 23:35:53 +0700

    add: gen generate_id

---
+++ app/utils/generate.py
@@ -0,0 +1,23 @@
+import os
+import random
+import time
+
+def generate_id(length=10):
+    """Generates a unique ID string using integers only.
+
+    Args:
+        length (int, optional): The desired length of the ID string. Defaults to 10.
+
+    Returns:
+        str: The generated ID string.
+    """
+
+    # Generate a random integer seed based on current time and process ID
+    seed = int(time.time() * 1000) + os.getpid()
+    random.seed(seed)
+
+    # Generate a random integer within the desired range
+    id_int = random.randint(10 ** (length - 1), 10 ** length - 1)
+
+    # Convert the integer to a string and return it
+    return str(id_int)
