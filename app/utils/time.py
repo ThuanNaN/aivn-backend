f003640357a34db47c19d84484f48bc3341918dd
ThuanNaN <duongthuan1445Gmail.com>
2024-09-25 11:02:41 +0700

    add: local to utc

--- app/utils/time.py
+++ app/utils/time.py
@@ -11,6 +11,8 @@ def utc_to_local(utc_time: datetime.datetime) -> str:
     local_time = utc_time + hcm_difference
     return local_time.isoformat()
 
+def local_to_utc(local_time: str) -> datetime.datetime:
+    return datetime.datetime.fromisoformat(local_time) - hcm_difference
 
 def str_to_datetime(str_datetime: str) -> datetime.datetime:
     return datetime.datetime.strptime(str_datetime, '%Y-%m-%dT%H:%M:%S.%f')
