87e39b35bb1ecc8b8aced7621506f2506cc0464c
ThuanNaN <duongthuan1445Gmail.com>
2024-09-30 08:22:07 +0700

    add verify router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -8,6 +8,7 @@ from app.api.v1.routes.problem import router as problem_router
 from app.api.v1.routes.code import router as code_router
 from app.api.v1.routes.submission import router as submission_router
 from app.api.v1.routes.retake import router as retake_router
+from app.api.v1.routes.verify import router as verify_router
 from app.api.v1.routes.sleep import router as sleep_router
 from app.core.security import (
     is_authenticated,
@@ -61,6 +62,10 @@ router.include_router(retake_router,
                       dependencies=[Depends(is_admin)],
                       prefix="/retake",
                       tags=["Retake"])
+
+router.include_router(verify_router,
+                      prefix="/verify",
+                      tags=["Verify"] )
 
 router.include_router(sleep_router,
                       dependencies=[Depends(is_admin)],
