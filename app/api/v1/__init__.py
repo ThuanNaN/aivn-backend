806f583f9003b89fdea99c0340f0a5fe25184ec2
ThuanNaN <duongthuan1445Gmail.com>
2024-10-01 15:59:59 +0700

    update routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,3 +1,4 @@
+from app.core.config import settings
 from fastapi import APIRouter, Depends
 from app.api.v1.routes.visualize import router as visualize_router
 from app.api.v1.routes.user import router as user_router
@@ -15,11 +16,11 @@ from app.core.security import (
     is_aio,
     is_admin
 )
-
 router = APIRouter()
 
+# Public User Routes
 router.include_router(visualize_router,
-                      dependencies=[Depends(is_aio)],
+                      dependencies=[Depends(is_authenticated)],
                       prefix="/visualize",
                       tags=["Visualize"])
 
@@ -28,46 +29,58 @@ router.include_router(user_router,
                       prefix="/user",
                       tags=["User"])
 
-router.include_router(contest_router,
-                      dependencies=[Depends(is_aio)],
-                      prefix="/contest",
-                      tags=["Contest"])
+router.include_router(verify_router,
+                      prefix="/verify",
+                      tags=["Verify"])
 
-router.include_router(exam_router,
-                      dependencies=[Depends(is_aio)],
-                      prefix="/exam",
-                      tags=["Exam"])
-
-router.include_router(category_router,
-                      dependencies=[Depends(is_aio)],
-                      prefix="/category",
-                      tags=["Category"])
-
-router.include_router(problem_router,
-                      dependencies=[Depends(is_aio)],
-                      prefix="/problem",
-                      tags=["Problem"])
-
-router.include_router(code_router,
-                      dependencies=[Depends(is_aio)],
-                      prefix="/code",
-                      tags=["Code"])
-
-router.include_router(submission_router,
-                      dependencies=[Depends(is_aio)],
-                      prefix="/submission",
-                      tags=["Submission"])
-
+# Admin Routes
 router.include_router(retake_router,
                       dependencies=[Depends(is_admin)],
                       prefix="/retake",
                       tags=["Retake"])
 
-router.include_router(verify_router,
-                      prefix="/verify",
-                      tags=["Verify"] )
-
 router.include_router(sleep_router,
                       dependencies=[Depends(is_admin)],
                       prefix="/sleep",
                       tags=["Sleep"])
+
+# Dev (admin only) Routes
+if settings.ENV_TYPE == "development":
+    AIO_DEPENDENCIES = [Depends(is_admin)]
+
+# Production Routes
+elif settings.ENV_TYPE == "production":
+    AIO_DEPENDENCIES = [Depends(is_aio)]
+
+else:
+    raise ValueError("Invalid Environment Type in .env file")
+
+router.include_router(contest_router,
+                      dependencies=AIO_DEPENDENCIES,
+                      prefix="/contest",
+                      tags=["Contest"])
+
+router.include_router(exam_router,
+                      dependencies=AIO_DEPENDENCIES,
+                      prefix="/exam",
+                      tags=["Exam"])
+
+router.include_router(category_router,
+                      dependencies=AIO_DEPENDENCIES,
+                      prefix="/category",
+                      tags=["Category"])
+
+router.include_router(problem_router,
+                      dependencies=AIO_DEPENDENCIES,
+                      prefix="/problem",
+                      tags=["Problem"])
+
+router.include_router(code_router,
+                      dependencies=AIO_DEPENDENCIES,
+                      prefix="/code",
+                      tags=["Code"])
+
+router.include_router(submission_router,
+                      dependencies=AIO_DEPENDENCIES,
+                      prefix="/submission",
+                      tags=["Submission"])
