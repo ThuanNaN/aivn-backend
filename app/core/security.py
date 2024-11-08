ad20e7fbb4f9da9b209a36513373f3d4231d2b90
ThuanNaN <duongthuan1445@gmail.com>
2024-11-08 09:08:36 +0700

    add prod and dev key

--- app/core/security.py
+++ app/core/security.py
@@ -6,10 +6,24 @@ from jwt.exceptions import (
 )
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer
+from app.core.config import settings
 from app.api.v1.controllers.user import retrieve_user
 
 
-PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+if settings.ENV_TYPE == "development":
+    PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvZrZXYBd+sjK1wQ3t2yH
+QdhRL8gOGJrgkuNqW30dRmWShRQ6/UCybp1ojgJKW3P31o8fLhuOCnKCjAjDPI0X
+QbtjajAKcoCUEcx38VHLv2gT5STIhgvSC9IrDEOflmFz6+5KbQQcQBvyV51V9eb7
+FaeGRe9CIRlMiO+tKXqmx3Lt0Vnw+5GuvLV5/nWQspElpn1pYRMYmI0lh6/KFLQ0
+PaYSLZenUk1UfGiLbgVcuv9Aht1n7wdb2SmGgIcTVqhYnUGNzsXKCglRZ7A6bH1n
+pklFqYoNwF4Mps7hKQ1D/ddxAGEaASNwMam5pMt2ZeOohOmX5BNQ7YuGzyjf+KZD
+ywIDAQAB
+-----END PUBLIC KEY-----
+"""
+
+elif settings.ENV_TYPE == "production":
+    PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsZ/vvCETUyrlqfnKpCfN
 fZhOrColBWz3rpjBSf3zDuZm5APCVUdI19ETgPPxKJBRaa8s9ij0BrIIFDU3ARnd
 fonsWZpxbdFG3wfWOPj+rRMDVcxpcWnhkdIHUKO/AiJqb9+nsCDa29njW3Co+O4u
@@ -20,6 +34,8 @@ pQIDAQAB
 -----END PUBLIC KEY-----
 """
 
+else:
+    raise ValueError("Invalid ENV_TYPE")
 
 ALGORITHM = "RS256"
 oauth2_scheme = HTTPBearer()
