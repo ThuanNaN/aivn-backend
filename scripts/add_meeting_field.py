21977b5335b3ffc00ab45a3dfdcac5115e02f1bc
ThuanNaN <duongthuan1445@gmail.com>
2024-11-15 13:41:38 +0700

    add meeting script

--- scripts/add_meeting_field.py
+++ scripts/add_meeting_field.py
@@ -9,7 +9,7 @@ except Exception as e:
     exit(1)
 
 
-async def main():
+async def add_record_field():
     all_meeting_data = []
     async for meeting_data in meeting_collection.find():
         all_meeting_data.append(meeting_data)
@@ -28,5 +28,29 @@ async def main():
         await meeting_collection.bulk_write(operators)
     print("Done")
 
+
+async def add_join_fields():
+    all_meeting_data = []
+    async for meeting_data in meeting_collection.find():
+        all_meeting_data.append(meeting_data)
+    
+    operators = []
+    with tqdm(total=len(all_meeting_data)) as pbar:
+        for index, meeting_data in enumerate(all_meeting_data):
+            operators.append(UpdateOne(
+                {"_id": meeting_data["_id"]},
+                {"$set": {
+                    "join_link": None,
+                    "join_code": None
+                }}
+            ))
+            pbar.update(1)
+    if operators:
+        await meeting_collection.bulk_write(operators)
+    print("Done")
+
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(add_record_field())
+    asyncio.run(add_join_fields())
