63ab661ce954e05d784722b6b7189802e7e5cb1b
ThuanNaN <duongthuan1445@gmail.com>
2024-11-28 04:10:11 +0700

    add code to MessageException

--- app/utils/exception.py
+++ app/utils/exception.py
@@ -1,8 +1,11 @@
 
 class MessageException(Exception):
-    def __init__(self, message="An error occurred"):
+    def __init__(self, 
+                 message="An error occurred", 
+                 status_code: int | None = None):
         super().__init__(message)
         self.message = message
+        self.status_code = status_code
 
     def __str__(self):
         return self.message
