546c7007e2fce26929a0a6af6f3a021b5ee92f48
ThuanNaN <duongthuan1445@gmail.com>
2025-01-14 16:51:38 +0700

    update: PROJECT_NAME

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Python Coding API"
+    PROJECT_NAME: str = "aivnlearning"
     MONGODB_URI: str = os.getenv("MONGODB_URI")
     ENV_TYPE: str = os.getenv("ENV_TYPE")
     FRONTEND_URL: str = os.getenv("FRONTEND_URL")
