a43252f27a8837c97ee86529ec6d645365a38bfe
ThuanNaN <duongthuan1445@gmail.com>
2025-01-16 04:52:00 +0700

    update: setting config

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,15 @@ load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "aivnlearning"
-    MONGODB_URI: str = os.getenv("MONGODB_URI")
     ENV_TYPE: str = os.getenv("ENV_TYPE")
-    FRONTEND_URL: str = os.getenv("FRONTEND_URL")
     OPENAPI_URL: str | None = "/v1/openapi.json" if ENV_TYPE == "development" else None
     DOCS_URL: str | None = "/v1/docs" if ENV_TYPE == "development" else None
+    MONGODB_URI: str = os.getenv("MONGODB_URI")
+    CLERK_SECRET_KEY: str = os.getenv("CLERK_SECRET_KEY")
+    FRONTEND_URL: str = os.getenv("FRONTEND_URL")
+    RESEND_API_KEY: str = os.getenv("RESEND_API_KEY")
+    INNGEST_SIGNING_KEY: str = os.getenv("INNGEST_SIGNING_KEY")
+    INNGEST_EVENT_KEY: str = os.getenv("INNGEST_EVENT_KEY")
+
 
 settings = Settings()
