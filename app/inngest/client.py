5a4d87bfbf16dd26bbbcbd96b08ee1b2a9c6d460
ThuanNaN <duongthuan1445@gmail.com>
2025-01-16 14:27:16 +0700

    update inngest env

--- app/inngest/client.py
+++ app/inngest/client.py
@@ -5,4 +5,11 @@ from app.core.config import settings
 logger = logging.getLogger("uvicorn.inngest")
 logger.setLevel(logging.DEBUG)
 
-inngest_client = inngest.Inngest(app_id=settings.PROJECT_NAME, logger=logger)
+inngest_client = inngest.Inngest(
+    app_id=settings.PROJECT_NAME,
+    env=settings.ENV_TYPE,
+    is_production=settings.ENV_TYPE == "production",
+    signing_key=settings.INNGEST_SIGNING_KEY,
+    event_key=settings.INNGEST_EVENT_KEY,
+    logger=logger
+)
